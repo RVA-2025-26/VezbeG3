30404d9f1ec64ac398b08207ad7a2133d9e02c46
VladimirFabri98 <vfficial98@gmail.com>
2026-05-18 10:02:58 +0200

    Vezbe 7 - App component - 18.05.2026.

--- BackendProject/src/main/java/rva/controllers/ArtiklController.java
+++ BackendProject/src/main/java/rva/controllers/ArtiklController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import rva.model.Artikl;
 import rva.services.ArtiklService;
 
 @RestController
+@CrossOrigin
 public class ArtiklController {
 
 	@Autowired
--- BackendProject/src/main/java/rva/controllers/StavkaPorudzbineController.java
+++ BackendProject/src/main/java/rva/controllers/StavkaPorudzbineController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,14 +16,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import rva.model.Artikl;
-import rva.model.Porudzbina;
 import rva.model.StavkaPorudzbine;
-import rva.services.ArtiklService;
-import rva.services.PorudzbinaService;
 import rva.services.StavkaPorudzbineService;
 
 @RestController
+@CrossOrigin
 public class StavkaPorudzbineController {
 
 	@Autowired
